16b12297c819a891a74618f5d7d8b57df79057e5
Glenn Contreras <GlennRC@Glenns-MacBook-Pro.local>
2015-03-24 11:14:30 -0700

    You can see songs and artist now

---
+++ VertMusic/SongTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SongTableViewController.h
+//  VertMusic
+//
+//  Created by Glenn Contreras on 3/22/15.
+//  Copyright (c) 2015 Vert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SongTableViewController : UITableViewController
+
+@end
---
+++ VertMusic/SongTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  SongTableViewController.m
+//  VertMusic
+//
+//  Created by Glenn Contreras on 3/22/15.
+//  Copyright (c) 2015 Vert. All rights reserved.
+//
+
+#import "SongTableViewController.h"
+
+@implementation SongTableViewController
+
+@end
